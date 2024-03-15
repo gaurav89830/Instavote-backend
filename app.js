ff35c2fd7a814ccbfeb01fc10c98fb27dac12257
Gaurav Sharma <gaurav89830@gmail.com>
2024-03-15 07:00:29 +0530

    socket.io setup

--- app.js
+++ app.js
@@ -1,20 +1,44 @@
 import express from "express";
 import { Server } from "socket.io";
-import createServer from "http";
+import { createServer } from "http";
+import cors from "cors";
 
 const port = 3000;
 const app = express();
-const server = createServer(app); // server created using http
-const io = new Server(server, {}); // this represents the whole circuit
+const server = createServer(app); 
 
-app.listen(port, () => {
-  console.log("Server is active at " + port);
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:5173",
+    methods: ["GET", "POST"],
+    credentials: true,
+  },
 });
+
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    methods: ["GET", "POST"],
+    credentials: true,
+  })
+);
 
 app.get("/", (req, res) => {
   res.send("Hello world");
 });
 
+server.listen(port, () => {
+  console.log("Server is active at " + port);
+});
+
 io.on("connection", (socket) => {
   console.log("User got connected with id : " + socket.id);
+
+  socket.emit("welcome", "Welcome to the server baby!");
+  socket.on("disconnect", () => {
+    console.log("User disconnected", socket.id);
+  });
+  socket.on("chat", (data) => {
+    socket.broadcast.emit("receiveChat", data);
+  });
 });
