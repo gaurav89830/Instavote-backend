da9d79fdf87c33ef033058e581484a7bccc30efd
Gaurav Sharma <gaurav89830@gmail.com>
2024-03-16 06:31:59 +0530

    test desploy

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const server = createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5173",
+    origin: "*",
     methods: ["GET", "POST"],
     credentials: true,
   },
@@ -20,7 +20,7 @@ const io = new Server(server, {
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "*",
     methods: ["GET", "POST"],
     credentials: true,
   })
